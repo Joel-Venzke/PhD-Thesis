d2464f8f057e54d86bc6b7f7a1f7e6e26512bdad
Joel Venzke <joel.a.venzke@gmail.com>
2020-09-23 11:47:20 -0600

    Starting with current draft

---
+++ figs/Photo_ionization/GAP/copy_figs.sh
@@ -0,0 +1,20 @@
+# plotting software is asym_vs_I in the same folder as the figure
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/conseptual_figs/integral_figs/integral_combined.pdf .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/conseptual_figs/selection_rules/combine_fig_1.pdf .
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/He/data/2p1/He_2p1.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/He/data/2p1/He_2p1_detune_1p2.eps .
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/He/data/2p1/CEP/He_2p1_asym_alpha.eps .
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/2p-1_3d0/Ne_2p-1_3d0.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/2p0_3d1/Ne_2p0_3d1.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/2p1_3d2/Ne_2p1_3d2.eps .
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/realistic/1-photon/2p-1_3d0/Ne_2p-1_3d0_real.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/realistic/1-photon/2p0_3d1/Ne_2p0_3d1_real.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/realistic/1-photon/2p1_3d2/Ne_2p1_3d2_real.eps .
+
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/realistic/1-photon/combine/Ne_2p-1_3d0_1p-combined.eps .
+cp /Users/joelvenzke/CU/RA/Data/PAD_asym/calculations/Ne/data/realistic/3-photon/combine/Ne_2p-1_3d2_3p-combined.eps .
